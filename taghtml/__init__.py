db2a1af44dd070bd8d2945ee91c9b5b019d16b34
Malte Ihlefeld <Malte.Ihlefeld@gls-itservices.com>
2025-09-24 14:48:20 +0200

    add jinja as dependency

--- taghtml/__init__.py
+++ taghtml/__init__.py
@@ -1,4 +1,4 @@
-"""A simple python script for generating highly customizable competitino name tags based on an HTML/CSS template.
+"""A simple python script for generating highly customizable competition name tags based on an HTML/CSS template.
 """
 
 __version__ = "0.1.0"
